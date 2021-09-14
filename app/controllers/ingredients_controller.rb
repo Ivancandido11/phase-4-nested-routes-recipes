d981f2565e4bea454fa9932dbae5d00a73a98f90
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-14 16:11:57 -0400

    Completed Assignment

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,8 +1,33 @@
 class IngredientsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_no_response_found
 
   def index
-    ingredients = Ingredient.all
+    if params[:recipe_id]
+      recipe = Recipe.find(params[:recipe_id])
+      ingredients = recipe.ingredients
+    else
+      ingredients = Ingredient.all
+    end
     render json: ingredients, include: :recipe
   end
 
+  def show
+    ingredient = Ingredient.find(params[:id])
+    render json: ingredient
+  end
+
+  def create
+    ingredient = Ingredient.create(ingredient_params)
+    render json: ingredient, status: :created
+  end
+
+private
+
+  def render_no_response_found
+    render json: { errors: "No ingredients found" }, status: :not_found
+  end
+
+  def ingredient_params
+    params.permit(:name, :quantity, :recipe_id)
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,14 @@
 class RecipesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_no_response_found
 
   def show
     recipe = Recipe.find_by(id: params[:id])
     render json: recipe, include: :ingredients
   end
 
+private
+
+  def render_no_response_found
+    render json: { errors: "No recipes found" }, status: :not_found
+  end
 end
